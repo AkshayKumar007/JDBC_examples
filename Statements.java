d3e6f76698b9492fa5b3d589e0b754f1d63cfad3
Akshay Kumar <35987800+AkshayKumar007@users.noreply.github.com>
2019-10-15 21:46:53 +0530

    Update Statements.java

--- Statements.java
+++ Statements.java
@@ -3,11 +3,16 @@ class Statements{
     public static void main(String args[]) throws Exception{  
         // making connections
         Class.forName("oracle.jdbc.driver.OracleDriver");  
-        Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","Naruto kawasaki","abcd#1234");  
+        Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","Angel Priya","abcd#1234");  
         PreparedStatement stmt = con.prepareStatement();  
         
         //insert query
-        stmt.executeUpdate("insert into emp765 values(33,'Irfan',50000)");  
+        stmt.executeUpdate("insert into emp765 values(?,?,?)");  //  (33,'Tourist',50000)
+        //setting values
+        stmnt.clearParameters();
+        stmnt.setInteger(1, 33);
+        stmnt.setString(2, 'Tourist');
+        stmnt.setInteger(3, 50000);
         
         //update query
         int result1 = stmt.executeUpdate("update emp765 set name='Vimal',salary=10000 where id=33"); 
@@ -15,7 +20,7 @@ class Statements{
         //delete query
         int result2 = stmt.executeUpdate("delete from emp765 where id=33");  
         
-        System.out.println(result + " records affected");  
+        System.out.println(result1 + " records affected");  
         
         con.close();  
     }
