3c4f88e8b65b3c85a540c46aa16ff1a13937fcc6
Akshay Kumar <35987800+AkshayKumar007@users.noreply.github.com>
2019-10-15 21:34:41 +0530

    Update Connections.java

--- Connections.java
+++ Connections.java
@@ -16,13 +16,14 @@ class Connections{
              
             
             //step3 create the statement object  
-            Statement stmt=con.createStatement();  
+            Statement stmt = con.createStatement();  
             
             //step4 execute query  
-            ResultSet rs=stmt.executeQuery("select * from emp");  
-            while(rs.next())  
-            System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-            
+            ResultSet rs = stmt.executeQuery("select * from emp");  
+            while(rs.next()) {
+                System.out.println( rs.getInt(1) + "  "+rs.getString(2) + "  " + rs.getString(3));  
+            }
+                            
             //step5 close the connection object  
             con.close();  
         
