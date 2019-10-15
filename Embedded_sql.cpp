934fa343d78b626c9b7bcd6aaeda19809deae24e
AkshayKumar007 <akshaykrcc@live.com>
2019-10-15 19:42:25 +0530

    added embedded sql example

---
+++ Embedded_sql.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+// #include <sql.h> 
+using namespace std;
+int main() {  
+   EXEC SQL INCLUDE SQLCA;  
+   EXEC SQL BEGIN DECLARE SECTION;  
+      int OrderID;         /* Employee ID (from user)         */  
+      int CustID;            /* Retrieved customer ID         */  
+      char SalesPerson[10]   /* Retrieved salesperson name      */  
+      char Status[6]         /* Retrieved order status        */  
+   EXEC SQL END DECLARE SECTION;  
+  
+   /* Set up error processing */  
+   EXEC SQL WHENEVER SQLERROR GOTO query_error;  
+   EXEC SQL WHENEVER NOT FOUND GOTO bad_number;  
+  
+   /* Prompt the user for order number */  
+   printf ("Enter order number: ");  
+   scanf_s("%d", &OrderID);  
+  
+   /* Execute the SQL query */  
+   EXEC SQL SELECT CustID, SalesPerson, Status  
+      FROM Orders  
+      WHERE OrderID = :OrderID  
+      INTO :CustID, :SalesPerson, :Status;  
+  
+   /* Display the results */  
+   printf ("Customer number:  %d\n", CustID);  
+   printf ("Salesperson: %s\n", SalesPerson);  
+   printf ("Status: %s\n", Status);  
+   exit();  
+  
+query_error:  
+   printf ("SQL error: %ld\n", sqlca->sqlcode);  
+   exit();  
+  
+bad_number:  
+   printf ("Invalid order number.\n");  
+   exit();  
+}
